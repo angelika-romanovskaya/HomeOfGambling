0f9d1fdc005e9a72c78ac16945bd66620bb4114c
Angelika <angelika_romanovskaya_2002@mail.ru>
2023-09-03 18:09:24 +0300

    Change

--- script.js
+++ script.js
@@ -5,21 +5,18 @@ let burger = document.querySelector('.burger')
 let nav = document.querySelector('.nav')
 let home = document.querySelector('.home')
 
-window.addEventListener("resize", function(event){
-    event.preventDefault();
-    if (window.screen.width < 992) {
-        column1.forEach((item) => item.setAttribute('src', './icon/Drow.svg'))
-        column2.forEach((item) => item.setAttribute('src', './icon/Worldrow.svg'))
-        column3.forEach((item) => item.innerHTML = `<img src="./icon/row.svg" alt="row">
-                                                    <img src="./icon/Crown.svg" alt="Crown">`)
-     }
-     else {
-        column1.forEach((item) => item.setAttribute('src', './icon/D.svg'))
-        column2.forEach((item) => item.setAttribute('src', './icon/World.svg'))
-        column3.forEach((item) => item.innerHTML = `<img src="./icon/column.svg" alt="column">
-                                                    <img src="./icon/Crown.svg" alt="Crown">`)
-     }
-})
+if (window.screen.width < 992) {
+    column1.forEach((item) => item.setAttribute('src', './icon/Drow.svg'))
+    column2.forEach((item) => item.setAttribute('src', './icon/Worldrow.svg'))
+    column3.forEach((item) => item.innerHTML = `<img src="./icon/row.svg" alt="row">
+                                                <img src="./icon/Crown.svg" alt="Crown">`)
+ }
+ else {
+    column1.forEach((item) => item.setAttribute('src', './icon/D.svg'))
+    column2.forEach((item) => item.setAttribute('src', './icon/World.svg'))
+    column3.forEach((item) => item.innerHTML = `<img src="./icon/column.svg" alt="column">
+                                                <img src="./icon/Crown.svg" alt="Crown">`)
+ }
 
 burger.addEventListener('click', function(event){
     event.preventDefault();
